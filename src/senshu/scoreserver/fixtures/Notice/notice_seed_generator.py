432cbf2d1760fbc11ae7a1d1c7ac66b5e7c2bc94
tukejonny <tukejonny@gmail.com>
2016-06-08 07:36:14 +0900

    Add notice seed generator feature.

--- src/senshu/scoreserver/fixtures/Notice/notice_seed_generator.py
+++ src/senshu/scoreserver/fixtures/Notice/notice_seed_generator.py
@@ -4,16 +4,38 @@ import json
 import generator_template as gt
 import pprint
 
-def get_category_jsondata():
-    print("*"*20 + " Category " + "*"*20)
+MODEL_NAME = "scoreserver.notice"
+NUM = 10
+
+class Notice(object):
+    def __init__(self):
+        self.title = self.make_title()
+        self.description = self.make_description()
+
+    def make_title(self):
+        return gt.generate_randstr(15)
+
+    def make_description(self):
+        return gt.generate_randstr(100)
+
+    def __str__(self):
+        return "<Notice: {title}, {description}".format(
+            title=self.title,
+            description=self.description
+        )
+
+def get_notice_jsondata():
+    print("*"*20 + " Notice " + "*"*20)
     fixture_list = []
-    for pk, category in enumerate(categories):
-        fields = {
-            "name": category
-        }
-        question = gt.create_seeddict(MODEL_NAME, pk+1, **fields)
-        fixture_list.append(question)
+    for pk in range(NUM):
+        n = Notice()
+        notice = gt.create_seeddict(MODEL_NAME, pk, **n.__dict__)
+        fixture_list.append(notice)
     jsondata = json.dumps(fixture_list)
     pprint.pprint(jsondata)
     print("*"*20 + " Finish! " + "*"*20)
     return(jsondata)
+
+if __name__ == "__main__":
+    #pass
+    get_notice_jsondata()
--- src/senshu/scoreserver/fixtures/seed_generator.py
+++ src/senshu/scoreserver/fixtures/seed_generator.py
@@ -1,18 +1,22 @@
 import sys
 sys.path.append('Question/')
 sys.path.append('Category/')
+sys.path.append('Notice/')
 import json
 
 import question_seed_generator as qsg
 import category_seed_generator as csg
+import notice_seed_generator as nsg
 
 if __name__ == '__main__':
     jsondata = []
 
     #Category
     jsondata.extend(json.loads(csg.get_category_jsondata()))
-    #Questions
+    #Question
     jsondata.extend(json.loads(qsg.get_question_jsondata()))
+    #Notice
+    jsondata.extend(json.loads(nsg.get_notice_jsondata()))
 
     print(jsondata)
     with open("initial_data.json", "w") as f:
