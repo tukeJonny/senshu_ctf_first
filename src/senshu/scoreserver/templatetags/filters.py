f227729c38bfb71bf390ffb6cbaa853ae8c9511a
tukemenzamurai <tukejonny@gmail.com>
2017-02-08 01:17:08 +0900

    複数ヒントへの対応

--- src/senshu/scoreserver/templatetags/filters.py
+++ src/senshu/scoreserver/templatetags/filters.py
@@ -2,16 +2,26 @@
 
 from django import template
 from scoreserver.models import Hint
+from django.db.models.query import QuerySet
 
 register = template.Library()
 
 @register.filter(name='get_question_hint')
 def get_question_hint(question):
-    try:
-        hint = Hint.objects.get(question=question)
-        return hint.description
-    except Hint.DoesNotExist:
+    hints = Hint.objects.filter(question=question)
+    if len(hints) == 1:
+        hints = hints[0]
+    elif len(hints) == 0:
         return "No hint."
+    return hints
 
+@register.filter(name='iterate_hints')
+def iterate_hints(hints):
+    for hint in hints:
+        yield hint
+
+@register.filter(name='is_queryset')
+def is_queryset(obj):
+    return isinstance(obj, QuerySet)
 
 
